962197849d7a6808486a1e9340850af593e24ea8
Vince Broz <vince@broz.cc>
2015-04-06 12:55:39 -0400

    Make source_code_finder smart enough to find source code

--- lib/source_code_finder.rb
+++ lib/source_code_finder.rb
@@ -1,6 +1,16 @@
+require 'find'
+
 # Finds source code files in the current directory
 class SourceCodeFinder
+  def initialize(filefind: Find)
+    @filefind = filefind
+  end
+
   def find
-    []
+    files = []
+    @filefind.find('.') do |path|
+      files << path if path =~ /.*\.rb$/
+    end
+    files
   end
 end
--- spec/source_code_finder_spec.rb
+++ spec/source_code_finder_spec.rb
@@ -3,13 +3,20 @@ require 'spec_helper'
 require 'bigfiles'
 
 describe SourceCodeFinder do
-  subject(:source_file_finder) { SourceCodeFinder.new }
+  subject(:filefind) { double('filefind') }
+  subject(:source_file_finder) { SourceCodeFinder.new(filefind: filefind) }
 
   context 'With no files in directory' do
     describe '#find' do
-      it 'should find no files' do
-        files = []
+      it 'should find a ruby file' do
+        files = ['foo.rb']
+        expect(filefind).to receive(:find).with('.').and_yield('foo.rb')
         expect(source_file_finder.find).to eq(files)
+      end
+
+      it 'should not find a dummy file' do
+        expect(filefind).to receive(:find).with('.').and_yield('.dummy')
+        expect(source_file_finder.find).to eq([])
       end
     end
   end
