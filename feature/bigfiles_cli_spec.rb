3a3c2e603b1b30e2fe1aa4875bb4e83b6e9672d5
Vince Broz <vince@broz.cc>
2015-04-06 14:16:31 -0400

    Fix TravisCI break

--- feature/bigfiles_cli_spec.rb
+++ feature/bigfiles_cli_spec.rb
@@ -11,7 +11,7 @@ describe 'bigfiles' do
   %w(no_files three_files).each do |type|
     it "handles #{type} case" do
       expect(exec_io "cd feature/samples/#{type} &&" \
-                     'RUBYLIB=`pwd`/../../lib bigfiles -n 3')
+                     'RUBYLIB=`pwd`/../../lib:"$RUBYLIB" bigfiles -n 3')
         .to eq(IO.read("feature/expected/#{type}_results.txt"))
     end
   end
