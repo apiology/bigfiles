77b39ffd09f95c65047eb91cfe8831d2b24282d9
Vince Broz <vince@broz.cc>
2015-04-06 11:37:00 -0400

    nofiles -> no_files

--- feature/bigfiles_cli_spec.rb
+++ feature/bigfiles_cli_spec.rb
@@ -1,17 +1,15 @@
 require_relative 'feature_helper'
 
-# http://www.puzzlenode.com/puzzles/13-chess-validator
-
-describe 'Chess validator' do
+describe 'bigfiles' do
   it 'starts up with no arguments' do
     expect(exec_io 'bigfiles -h')
       .to eq("USAGE: bigfiles [-n <top n files>]\n")
   end
 
-  # simple complex_1 complex_2 complex_3 complex_4 complex_5 extra
-  %w(nofiles).each do |type|
+  # three_files one_file two_files many_files zero_byte_file
+  %w(no_files).each do |type|
     it "handles #{type} case" do
-      expect(exec_io "cd spec/samples/#{type} &&" \
+      expect(exec_io "cd feature/samples/#{type} &&" \
                      'bigfiles -n 3')
         .to eq(IO.read("spec/expected/#{type}_results.txt"))
     end
