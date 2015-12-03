b801ea91c12dd59420595ede14100ba5a1d0ca6c
Vince Broz <vince@broz.cc>
2015-12-03 16:13:01 -0500

    Add --num_files arg

--- spec/big_files/big_files_spec.rb
+++ spec/big_files/big_files_spec.rb
@@ -14,14 +14,18 @@ describe BigFiles::BigFiles do
   end
 
   [{ glob: nil, exclude: nil },
-   { glob: '*/*.{rb,swift}', exclude: '*/foo.rb' }].each do |config|
+   { glob: '*/*.{rb,swift}', exclude: '*/foo.rb' },
+   { glob: '*/*.{rb,swift}', exclude: '*/foo.rb',
+     num_files: '6' }].each do |config|
     glob = config[:glob]
     exclude_glob = config[:exclude]
+    num_files = config[:num_files]
     context "With glob of #{glob}" do
       subject(:args) do
         args = []
         args += ['--glob', glob] unless glob.nil?
         args += ['--exclude', exclude_glob] unless exclude_glob.nil?
+        args += ['--num-files', num_files] unless num_files.nil?
         args
       end
 
@@ -42,8 +46,11 @@ describe BigFiles::BigFiles do
           expect(file_with_lines).to(receive(:new)).with(filename)
             .and_return(file)
           expect_file_queried(file, filename: filename, num_lines: num_lines)
-          expect(io).to receive(:puts).with("#{num_lines}: #{filename}")
           file
+        end
+
+        def expect_file_output(filename, num_lines)
+          expect(io).to receive(:puts).with("#{num_lines}: #{filename}")
         end
 
         def default_glob
@@ -63,7 +70,7 @@ describe BigFiles::BigFiles do
         end
 
         def expect_source_globber_used(glob, exclude_glob)
-          file_list = %w(file_1 file_2)
+          file_list = %w(file_1 file_2 file_3 file_4)
           expect_globs_assigned(glob, exclude_glob)
           expect(source_file_globber).to(receive(:source_files_arr))
             .and_return(file_list)
@@ -71,10 +78,32 @@ describe BigFiles::BigFiles do
 
         it 'runs' do
           expect_source_globber_used(glob, exclude_glob)
-          file_1 = expect_file_processed('file_1', 1)
-          file_2 = expect_file_processed('file_2', 2)
+          file_1 = expect_file_processed('file_1', 4)
+          file_2 = expect_file_processed('file_2', 3)
+          file_3 = expect_file_processed('file_3', 2)
+          file_4 = expect_file_processed('file_4', 1)
+
           allow(file_1).to receive(:<=>).with(file_2).and_return(1)
+          allow(file_1).to receive(:<=>).with(file_3).and_return(1)
+          allow(file_1).to receive(:<=>).with(file_4).and_return(1)
+
           allow(file_2).to receive(:<=>).with(file_1).and_return(-1)
+          allow(file_2).to receive(:<=>).with(file_3).and_return(1)
+          allow(file_2).to receive(:<=>).with(file_4).and_return(1)
+
+          allow(file_3).to receive(:<=>).with(file_1).and_return(-1)
+          allow(file_3).to receive(:<=>).with(file_2).and_return(-1)
+          allow(file_3).to receive(:<=>).with(file_4).and_return(1)
+
+          allow(file_4).to receive(:<=>).with(file_1).and_return(-1)
+          allow(file_4).to receive(:<=>).with(file_2).and_return(-1)
+          allow(file_4).to receive(:<=>).with(file_3).and_return(-1)
+
+          expect_file_output('file_1', 4)
+          expect_file_output('file_2', 3)
+          expect_file_output('file_3', 2)
+          expect_file_output('file_4', 1) if num_files && num_files.to_i >= 4
+
           bigfiles.run
         end
       end
