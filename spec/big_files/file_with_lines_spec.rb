7b697bb3c6f172b62172c83ded1216902eb0fd2f
Vince Broz <vince@broz.cc>
2019-12-29 14:25:49 -0500

    Fix RuboCop issues

--- spec/big_files/file_with_lines_spec.rb
+++ spec/big_files/file_with_lines_spec.rb
@@ -9,7 +9,7 @@ describe BigFiles::FileWithLines do
     described_class.new(filename, file_opener: file_opener)
   end
 
-  context 'With a filename' do
+  context 'with a filename' do
     describe '#new' do
       it 'initializes' do
         file_with_lines
@@ -18,15 +18,15 @@ describe BigFiles::FileWithLines do
 
     describe '#<=>' do
       let_double :spaceship_result, :another_filename
-      subject(:another_file_with_lines) do
+      let(:another_file_with_lines) do
         described_class.new(another_filename, file_opener: file_opener)
       end
 
       it 'is sortable' do
-        expect(file_with_lines).to(receive(:num_lines))
-                               .and_return(1)
-        expect(another_file_with_lines).to(receive(:num_lines))
-                                       .and_return(2)
+        allow(file_with_lines).to(receive(:num_lines))
+                              .and_return(1)
+        allow(another_file_with_lines).to(receive(:num_lines))
+                                      .and_return(2)
         expect(file_with_lines.<=>(another_file_with_lines))
           .to eq(-1)
       end
@@ -38,11 +38,11 @@ describe BigFiles::FileWithLines do
       it 'returns a number' do
         # file=File.open("path-to-file","r")
         # file.readlines.size
-        expect(file_opener).to(receive(:open)).with(filename, 'r')
-                           .and_yield(opened_file)
-        expect(opened_file).to(receive(:each_line))
-                           .and_yield('line 1').and_yield('line 2')
-                           .and_yield('line 3')
+        allow(file_opener).to(receive(:open)).with(filename, 'r')
+                          .and_yield(opened_file)
+        allow(opened_file).to(receive(:each_line))
+                          .and_yield('line 1').and_yield('line 2')
+                          .and_yield('line 3')
         expect(file_with_lines.num_lines).to eq(3)
       end
     end
